0cd98b4bf3134bdf4164ddf6518b0d8da8f6fe1a
Meghanad Shingate <Meghanad Shingate @LAB>
2011-12-23 18:30:19 +0530

    doc+info

--- PK1367.py
+++ PK1367.py
@@ -1,6 +1,64 @@
 #################################################
 #			*POKER*
 #
+#	1.	People (code :))
+#	2.	Probability
+#	3.	Money
+#
+#
+#	Positions:
+#
+#			1	2
+#		6			3
+#			5	4
+#
+#
+#
+#	Rank of hand:
+#
+#		One pair (low)
+#		Two Pair
+#		Three of Kind - set/trips
+#		Straight - of any kinds
+#		Flush	-	five cards of same suit
+#		Full house	-	three of kind + pair
+#		Four of Kind
+#		straight flush	-	
+#		royal flush	(high)-	10-j-q-k-a
+#
+#
+#	Terms:
+#		dealer
+#		small blind
+#		big blind
+#		hole cards	-	pair cards given to us(player)
+#		flop - three cards face-up, can be used by any player at shutdown.
+#		turn - forth card face-up
+#		river - fifth card face-up
+#		shutdown- create best five-card hand possible
+#
+#
+#
+#
+#	Actions:
+#		1. Call	- match amount bet
+#
+#
+#
 #	decisions:
 #	==========
 #
+#
+#	Files:
+#	======
+#	inputf.txt	-	
+#
+#	deals_money.txt	-	
+#
+#	deal.txt	-	
+#
+#	outputf.txt	-	
+#
+#
+#	Ref: http://entertainment.howstuffworks.com/poker.htm
+
