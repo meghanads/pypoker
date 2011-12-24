8267689126265e80315000215b9e9b8b42e9a432
Meghanad Shingate <me@gmail.com>
2011-12-25 04:35:03 +0530

    PyGame - initialized

--- PK1367.py
+++ PK1367.py
@@ -66,7 +66,7 @@
 
 from math import *
 from enum import *
-from os import *
+import os
 import os.path
 
 
@@ -89,6 +89,9 @@ decision = Enum('fold', 'call', 'raise');
 # stages in game
 stage= Enum('Pre_Flop', 'Flop', 'Fourth_Street', 'Fifth_Street', 'Show_Down');
 
+NameStage = { 1 : 'Pre_Flop', 2 : 'Flop', 3 : 'Fourth_Street', 4 : 'Fifth_Street', 5 : 'Show_Down'};
+NumStage = { 'Pre_Flop' : 1, 'Flop' : 2, 'Fourth_Street' : 3, 'Fifth_Street' : 4 , 'Show_Down' : 5};
+
 # Rank of hand:
 rank = Enum('one_pair', 'two_pairs', 'set', 'straight', 'flush', 'full_house', 'four_of_kind', 'straight_flush', 'royal_flush');
 
@@ -100,77 +103,118 @@ opp_charc = Enum('bluff', 'simple', 'agressive');
 # Game info:
 #	Holds games status
 
+
 # megh_game
-# ===============
+# =======================
 # new_game
+
+
 
 # megh_deal
 # =======================
-# new_deal
+# deal_num
 #
+
+NW_DEAL = 0;	# new_deal?
+NW_GAME = 0;
+
 class PyGame:
 	
 	def _init_(self):
 		self.smallblind = 0;
 		self.bigblind = 0;
-		pot = 0;		# pot of game
-		min_raise = 0;
-		new_deal = 0;	# is it new deal??
-		deal_num = 0;	# deal number
-		new_game = 0;
+		self.dealer = 0;
+		self.main_pot = 0;		# pot of game
+		self.stage = 0;
+		self.min_raise = 0;
+		self.new_deal = 0;	# is it new deal??
+		self.deal_num = 0;	# deal number
+		self.new_game = 0;
 
-		card = [0,0,0,0,0,0,0];	# cards can be seen by me
+		self.card = [0,0,0,0,0,0,0];	# cards can be seen by me
 
-		
+		# is new game?
+		if(os.path.isfile('megh_game')):
+			# game already started...
+			self.new_game = 0;
+
+		else:
+			# game just started
+			# remove player_stat files if exists
+			if(os.path.isfile('megh_player1_stat'):
+					os.remove('megh_player1_stat');
+
+			if(os.path.isfile('megh_player2_stat'):
+					os.remove('megh_player2_stat');
+
+			if(os.path.isfile('megh_player3_stat'):
+					os.remove('megh_player3_stat');
+
+			if(os.path.isfile('megh_player4_stat'):
+					os.remove('megh_player4_stat');
+
+			if(os.path.isfile('megh_player5_stat'):
+					os.remove('megh_player5_stat');
+
+			f = open('megh_game','w');
+			f.write(str(0));
+			f.write('\n');
+			f.close();
+
+			self.new_game = 1;
+
+								
+	
+
+
+
 		# is it new deal??
-		
 		if(os.path.isfile('megh_deal')):
 			# if file exists... old deal
+			self.new_deal = 0;	# old deal
+			f = open('megh_deal','r');
+			lin = f.readline();
+			self.deal_num = int(lin.strip());
+
+			
 
 		else:
 			# new deal
-			
-
-		try:
-			# megh_deal -> created at start of each deal and removed on show_down
-			f = open('megh_deal');
-			break;
-		except IOError:
-			# file does not exists...new deal just started.
-			# cards are unknown
-			new_deal = 1;	# new deal
 			# find deal num
+			self.new_deal = 1;
 			f = open('deals_money.txt');
 			lin = f.readline();
 			fwords = lin.split(' ');
-			deal_num = int(fword[1]);
+			w = fwords[1];
+			self.deal_num = int(w.strip());
+			f.close();
 
-			# in new deal cards are different but players are SAME
-			
+			f = open('megh_deal', 'w');
+			f.write(str(self.deal_num));
+			f.write('\n');
+			f.close();
 
+		
+		f = open('deal.txt', 'r');
+		lin = f.readline();
+		fwords = lin.split(' ');
+		w = fwords[1];
+		self.dealer = int(w.strip());
+		self.smallblind = (self.delear+1)%7;
+		self.bigblind = (self.delear+2)%7;
 
+		lin = f.readline();
+		fwords = lin.split(' ');
+		w = fwords[1];
+		self.stage = NumStage[w.strip()];
 
-			just_strarted = 1;
+		lin = f.readline();
+		fwords = lin.split(' ');
+		w = fwords[1];
+		self.main_pot = int(w.strip());
 
-		if(not just_started):
-			# read file details and load...
-
-		else:
-			# read inputf.txt
-			#
-
-			
-
-
-		self.load_status();
-
-
-	def load_status(self) :	# load saved status
-
-
-
-	
-
+		NW_DEAL = self.new_deal;
+		NW_GAME = self.new_game;
 
 		
 
@@ -186,11 +230,49 @@ class My_Player :
 		self.money = 0;
 		self.mypos = 0;		# my position in game
 		self.hole = [0,0];	# hole cards
+		self.money_spent = 0;	# money spent in current deal
 		self.coc = 0;		# cost of call
 		self.load_status();
 
 		
 	def load_status(self) :	# load saved status
+		if(not NW_DEAL):
+			# old deal : load details of player from -> megh_deal
+
+		else:
+			# new deal : load details from -> inputf.txt, deals_money.txt
+			f = open('inputf.txt', 'r');
+			lin = f.readline();
+			fwords = lin.split(' ');
+			w = fwords[0];
+			self.hole[0] = int(w.strip());
+			w = fwords[1];
+			self.hole[1] = int(w.strip());
+			w = fwords[2];
+			self.mypos = int(w.strip());
+			w = fwords[3];
+			self.money_spent = int(w.strip());
+			f.close();
+
+			assert(self.mypos and self.mypos < 7);	# assert
+
+			f = open('deals_money.txt', 'r');
+			for i in range(self.mypos):
+				lin = f.readline();
+			lin = f.readline();
+			fwords = lin.split(' ');
+			w =fwords[1];
+			self.money = int(w.strip());
+
+
+
+
+
+
+
+
+
+			
 
 	def outs(self) :	# evaluating self's hand
 		# 1. Counting outs - cards still in deck that can give potentially winning hand
